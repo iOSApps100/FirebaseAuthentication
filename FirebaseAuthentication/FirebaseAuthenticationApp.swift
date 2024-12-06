c4da0c69af6efbbdcb1bc0861708142c972a4f95
iOSApps100 <contact.vikram92@gmail.com>
2024-12-06 20:24:32 +0530

    Initial Commit

---
+++ FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
