3c72850a9037062e273e0e9d2a3d00eb5d909f6e
iOSApps100 <contact.vikram92@gmail.com>
2024-12-10 22:16:27 +0530

    refactored all code & made sign up screen

---
+++ FirebaseAuthentication/Screens/Views/Styles/CapsuleButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CapsuleButtonStyle.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 10/12/24.
+//
+
+import SwiftUI
+
+struct CapsuleButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapsuleButtonStyle()
+}
