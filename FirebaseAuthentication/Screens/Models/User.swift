b724573ba3bfeb71645485a9b938476e794c11c0
iOSApps100 <contact.vikram92@gmail.com>
2024-12-12 22:41:57 +0530

    made Initialization of firebase and create first user

---
+++ FirebaseAuthentication/Screens/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 12/12/24.
+//
+
+import Foundation
---
+++ FirebaseAuthentication/Screens/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 12/12/24.
+//
+
+import Foundation
