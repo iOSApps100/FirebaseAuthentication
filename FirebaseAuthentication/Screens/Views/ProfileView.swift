a5863d4179f539ab0a0eff09816fb99b2f91e2a9
iOSApps100 <contact.vikram92@gmail.com>
2024-12-14 16:23:49 +0530

    SignOut functionality and profile view logic when user already loggedIn or not

---
+++ FirebaseAuthentication/Screens/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 14/12/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
