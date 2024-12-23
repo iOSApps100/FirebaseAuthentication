f497d7327e546e453388ab458cadce800a894422
iOSApps100 <contact.vikram92@gmail.com>
2024-12-23 15:10:08 +0530

    password reset screen and functionality done using firebase.

---
+++ FirebaseAuthentication/Screens/Views/EmailSentView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailSentView.swift
+//  FirebaseAuthentication
+//
+//  Created by Vivek  Garg on 23/12/24.
+//
+
+import SwiftUI
+
+struct EmailSentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailSentView()
+}
